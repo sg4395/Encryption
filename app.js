e1740611bfaa69c62de01cbaa460414d4e1cae0f
sg4395 <aki@MacBook-Pro.lan>
2021-09-05 01:23:41 +0800

    lv 6 OAuth Fb google

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const https=require("https");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
-
+const findOrCreate=require("mongoose-findorcreate");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
@@ -38,7 +40,9 @@ mongoose.set('useCreateIndex', true);
 //deprecataion fix
 const userSchema=new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
+  // since we now use OAuth
 });
 // const secret= "youwillneverguessthepasswordboy"; this is put into .env
 // seceret is our random string encryption key, we can also use generated key.
@@ -47,13 +51,74 @@ const userSchema=new mongoose.Schema({
 //encyrpted Fild specifies which field to encrypt, it takes an array.
 // mongoose encrypt encrpts when the save is used or the insert, and when we use find(), it decrypts the field.
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 //this plugin adds passportlocal mongoose to the mongoose schema called loginSchema.similar to lv1 and 2 encryption.;
 const User=mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());   these are for local mongoose Strategy
+//below is for all passport.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"gttps://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //access token
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+//passport google oAuth
 // these 3 lines is from passport-local-mongoose, serialize is the genertion of the cookie;
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_ID,
+    clientSecret: process.env.FB_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+  console.log(user);
+      return cb(err, user);
+
+    });
+  }
+));
+
+app.get("/auth/google",function(req,res){
+passport.authenticate("google", { scope: ["profile"] });
+});
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+
 app.get("/",function(req,res){
   res.render("home");
 });
@@ -83,7 +148,7 @@ app.get("/logout",function(req,res){
   req.logout();
   // logsout of session means cookie deleted.
   res.redirect("/");
-})
+});
 
 app.post("/register",function(req,res){
   // bcrypt.hash(req.body.password,10,function(err,hash){
@@ -110,6 +175,7 @@ app.post("/register",function(req,res){
 
 // this is registering user using Passport, User is the model connecting to mongoose, this method relies on passport=local-mongoose
 // this also doesnt allow duplicate registers.
+
     User.register({username:req.body.username},req.body.password,function(err,user){
       if (err){
         console.log(err);
@@ -134,7 +200,7 @@ app.post("/register",function(req,res){
 
 app.post("/login", function(req,res){
   // create new user, then use req.login from passport to authenticate the new user object.
-  //if no err, then passport authenticated and cookie is created and directed to secrets page.
+  // if no err, then passport authenticated and cookie is created and directed to secrets page.
   const user= new User({
     username:req.body.username,
     password:req.body.password
@@ -147,6 +213,8 @@ app.post("/login", function(req,res){
       });
     }
   });
+
+
 // var typedEmail=req.body.username;
 
 // Login.findOne({email:typedEmail},function(err,foundUser){
